57293c465b335ed3d5c656a3e3c25c1b097a0cf1
xany <635993292@qq.com>
2018-06-04 10:33:46 +0800

    扩展app.js基础方法

--- app.js
+++ app.js
@@ -4,15 +4,254 @@ App({
    * 全局变量
    */
   globalData: {
-    userInfo: null
+    // userInfo: null,
+    user_id: null,
   },
+
+  api_root: '', // api地址
+  siteInfo: require('siteinfo.js'),
 
   /**
    * 生命周期函数--监听小程序初始化
    */
   onLaunch: function () {
-    // this.getUserInfo();
+    // 设置api地址
+    this.setApiRoot();
   },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    // 获取小程序基础信息
+    this.getWxappBase();
+  },
+
+  /**
+   * 设置api地址
+   */
+  setApiRoot: function () {
+    let siteroot = this.siteInfo.siteroot.replace('app/index.php', '');
+    this.api_root = siteroot + 'addons/active_lite/source/web/index.php?s=/api/';
+  },
+
+  /**
+   * 获取小程序基础信息
+   */
+  getWxappBase: function (callback) {
+    let App = this;
+    App._get('wxapp/base', {}, function (result) {
+      if (result.code === 1) {
+        // 记录小程序基础信息
+        wx.setStorageSync('wxapp', result.data);
+        callback && callback();
+      } else {
+        App.showError(result.msg);
+      }
+    }, false, false);
+  },
+
+  /**
+   * 用户登录
+   * 自动检测当前登录态并实现微信登录
+   */
+  doLogin: function (callback) {
+    let App = this;
+    if (wx.getStorageSync('token').length !== 32) {
+      return App.wxLogin(callback);
+    }
+    // 自动检测登录
+    wx.checkSession({
+      success: function () {
+        callback && callback();
+      },
+      fail: function () {
+        // session_key 已经失效，需要重新执行登录流程
+        App.wxLogin(callback);
+      }
+    });
+  },
+
+  /**
+   * 微信登录
+   */
+  wxLogin: function (callback) {
+    let App = this;
+    wx.login({
+      success: function (res) {
+        App._post_form('user/login', { code: res.code }, function (result) {
+          if (result.code === 1) {
+            console.log(result.data);
+            wx.setStorageSync('token', result.data.token);
+            wx.setStorageSync('user_id', result.data.user_id);
+            callback && callback();
+          } else {
+            App.showError(result.msg);
+          }
+        });
+      }
+    });
+  },
+
+  /**
+   * 当前用户id
+   */
+  getUserId: function () {
+    return wx.getStorageSync('user_id');
+  },
+
+  /**
+   * 显示成功提示框
+   */
+  showSuccess: function (msg, callback) {
+    wx.showToast({
+      title: msg,
+      icon: 'success',
+      success: function () {
+        if (callback) {
+          setTimeout(function () {
+            callback();
+          }, 1500);
+        }
+      }
+    });
+  },
+
+  /**
+   * 显示失败提示框
+   */
+  showError: function (msg, callback) {
+    wx.showModal({
+      title: '友情提示',
+      content: msg,
+      showCancel: false,
+      success: function (res) {
+        if (callback) {
+          setTimeout(function () {
+            callback();
+          }, 1500);
+        }
+      }
+    });
+  },
+
+  /**
+   * get请求
+   */
+  _get: function (url, data, success, fail, check_login) {
+    wx.showNavigationBarLoading();
+    let App = this;
+    // 构造请求参数
+    data = data || {};
+    data.wxapp_id = App.siteInfo.uniacid;
+    if (typeof check_login === 'undefined')
+      check_login = true;
+
+    // 构造get请求
+    let request = function () {
+      data.token = wx.getStorageSync('token');
+      wx.request({
+        url: App.api_root + url,
+        header: {
+          'content-type': 'application/json'
+        },
+        data: data,
+        success: function (res) {
+          wx.hideNavigationBarLoading();
+          if (res.data.code === -1) {
+            // 登录态失效, 重新登录
+            App.wxLogin(function () {
+              App._get(url, data, success, fail, false);
+            });
+          } else {
+            if (success) success(res.data);
+          }
+        },
+        fail: function (res) {
+          wx.hideNavigationBarLoading();
+          if (fail) fail(res);
+        }
+      });
+    };
+    // 判断是否需要验证登录
+    check_login ? App.doLogin(request) : request();
+  },
+
+  /**
+   * post提交
+   */
+  _post_form: function (url, data, success, fail) {
+    let App = this;
+    data.wxapp_id = App.siteInfo.uniacid;
+    data.token = wx.getStorageSync('token');
+    wx.request({
+      url: App.api_root + url,
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      method: 'POST',
+      data: data,
+      success: function (res) {
+        if (res.data.code === -1) {
+          // 登录态失效, 重新登录
+          App.wxLogin(function () {
+            App._post_form(url, data, success, fail);
+          });
+        } else {
+          if (success) success(res.data);
+        }
+      },
+      fail: function (res) {
+        if (fail) fail(res);
+      }
+    });
+  },
+
+  /**
+   * 验证是否存在user_info
+   */
+  validateUserInfo: function () {
+    let user_info = wx.getStorageSync('user_info');
+    return !!wx.getStorageSync('user_info');
+  },
+
+  /**
+   * 对象转URL
+   */
+  urlEncode: function urlencode(data) {
+    var _result = [];
+    for (var key in data) {
+      var value = data[key];
+      if (value.constructor == Array) {
+        value.forEach(function (_value) {
+          _result.push(key + "=" + _value);
+        });
+      } else {
+        _result.push(key + '=' + value);
+      }
+    }
+    return _result.join('&');
+  },
+
+  /**
+   * 设置当前页面标题
+   */
+  setTitle: function () {
+    let App = this, wxapp;
+    if (wxapp = wx.getStorageSync('wxapp')) {
+      wx.setNavigationBarTitle({ title: wxapp.app_name });
+    } else {
+      App.getWxappBase(function () { App.setTitle(); });
+    }
+  },
+
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
   /**
    * 获取用户信息
@@ -63,7 +302,7 @@ App({
   /**
    * 执行登录
    */
-  doLogin: function (e, t) {
+  doLogin_bak: function (e, t) {
     var o = this;
     e && wx.request({
       url: o.apiUrl("ecapi.auth.weixin.mplogin"),
---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "小活动",
+    uniacid: "2",
+    acid: "1",
+    multiid: "0",
+    version: "1.0.0",
+    siteroot: "https://we7.cn/app/index.php",
+    design_method: "3"
+};
