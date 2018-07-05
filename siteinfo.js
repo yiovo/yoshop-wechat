350c5c6c48b7fe577116a18467e1d4648a3cf21e
xany <635993292@qq.com>
2018-07-05 21:26:03 +0800

    小程序配置文件

--- siteinfo.js
+++ siteinfo.js
@@ -1,10 +1,5 @@
 module.exports = {
-    name: "萤火小程序商城",
-    uniacid: "6",
-    acid: "1",
-    multiid: "0",
-    version: "1.0.0",
-    siteroot: "http://we7.cn/app/index.php",
-    // siteroot: "http://bestshop.cn/app/index.php",
-    design_method: "3"
+  name: "萤火小程序商城",
+  siteroot: "http://we7.cn/app/index.php", // 必填: api地址
+  uniacid: "6", // 必填: 微信小程序id
 };
