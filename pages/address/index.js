00aac994023c96a10d2c58c7dd0e67f8c637ce4f
萤火科技 <635993292@qq.com>
2018-07-31 09:10:44 +0800

    修复：小程序端设置默认收货地址问题

--- pages/address/index.js
+++ pages/address/index.js
@@ -6,12 +6,12 @@ Page({
     default_id: null,
   },
 
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 当前页面参数
     this.data.options = options;
   },
 
-  onShow: function () {
+  onShow: function() {
     // 获取收货地址列表
     this.getAddressList();
   },
@@ -19,9 +19,9 @@ Page({
   /**
    * 获取收货地址列表
    */
-  getAddressList: function () {
+  getAddressList: function() {
     let _this = this;
-    App._get('address/lists', {}, function (result) {
+    App._get('address/lists', {}, function(result) {
       if (result.code === 1) {
         _this.setData(result.data);
       } else {
@@ -33,14 +33,16 @@ Page({
   /**
    * 添加新地址
    */
-  createAddress: function () {
-    wx.navigateTo({ url: './create' });
+  createAddress: function() {
+    wx.navigateTo({
+      url: './create'
+    });
   },
 
   /**
    * 编辑地址
    */
-  editAddress: function (e) {
+  editAddress: function(e) {
     wx.navigateTo({
       url: "./detail?address_id=" + e.currentTarget.dataset.id
     });
@@ -49,14 +51,16 @@ Page({
   /**
    * 移除收货地址
    */
-  removeAddress: function (e) {
+  removeAddress: function(e) {
     let _this = this,
       address_id = e.currentTarget.dataset.id;
     wx.showModal({
       title: "提示",
       content: "您确定要移除当前收货地址吗?",
-      success: function (t) {
-        App._post_form('address/delete', { address_id }, function (result) {
+      success: function(t) {
+        App._post_form('address/delete', {
+          address_id
+        }, function(result) {
           if (result.code === 1) {
             _this.getAddressList();
           } else {
@@ -70,43 +74,22 @@ Page({
   /**
    * 设置为默认地址
    */
-  setDefault: function (e) {
+  setDefault: function(e) {
     let _this = this,
       address_id = e.detail.value;
-    App._post_form('address/setDefault', { address_id }, function (result) {
+    _this.setData({
+      default_id: parseInt(address_id)
+    });
+    App._post_form('address/setDefault', {
+      address_id
+    }, function(result) {
       if (result.code === 1) {
-        if (_this.data.options.from === 'flow') {
-          wx.navigateBack();
-        }
+        _this.data.options.from === 'flow' && wx.navigateBack();
       } else {
         App.showError(result.msg);
       }
     });
-
     return false;
-
-    // let a = this, s = t.detail.value, n = wx.getStorageSync("token");
-    // wx.request({
-    //   url: App.apiUrl("ecapi.consignee.setDefault"),
-    //   method: "POST",
-    //   header: {
-    //     "X-ECTouch-Authorization": n
-    //   },
-    //   data: {
-    //     consignee: s
-    //   },
-    //   success: function () {
-    //     wx.showToast({
-    //       title: "设置成功",
-    //       success: function () {
-    //         let e = wx.getStorageSync("pageOptions");
-    //         a.onLoad(e), e.from, wx.navigateBack({
-    //           delta: 1
-    //         });
-    //       }
-    //     });
-    //   }
-    // });
   },
 
 });
